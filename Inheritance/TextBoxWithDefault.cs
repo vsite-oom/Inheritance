e262d70beda51091194678bdea7a909849b6baa2
Ivan Batinić <ivek81cro@gmail.com>
2020-03-11 21:01:45 +0100

    Resolve TextBox control, set defualt text

---
+++ Inheritance/TextBoxWithDefault.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nasljeđivanje
+{
+    public partial class TextBoxWithDefault : TextBox
+    {
+        public TextBoxWithDefault()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            if (TextLength == 0)
+            {
+                DrawDefaultText(pe);
+            }
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            Invalidate();
+        }
+
+        private void DrawDefaultText(PaintEventArgs pe)
+        {
+            TextRenderer.DrawText(pe.Graphics, "Default", Font, ClientRectangle, SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+        }
+    }
+}
