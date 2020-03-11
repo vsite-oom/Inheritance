441bbddfd6847a20a675915927feef204c4968d7
Julijan Sribar <julijan.sribar@reversinglabs.com>
2020-03-11 15:27:11 +0100

    Add project files.

---
+++ Inheritance/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nasljeđivanje
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to cancel?", "Order Details") == DialogResult.OK)
+                this.Close();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
