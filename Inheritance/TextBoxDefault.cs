6d4348a591ec6106a88b081869f9b517e9bf2aea
Mateo Jadrušić <majadrus@vsite.hr>
2021-03-21 20:51:57 +0100

    Implement TextBoxDefault

---
+++ Inheritance/TextBoxDefault.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Nasljeđivanje
+{
+    class TextBoxDefault : System.Windows.Forms.TextBox
+    {
+        private string defaultText = "Default";
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (TextLength == 0)
+                DrawDefaulText(e) ;
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0) ;
+            Invalidate();
+        }
+        private void DrawDefaulText(PaintEventArgs e)
+        {
+            TextRenderer.DrawText(e.Graphics, defaultText, Font, ClientRectangle, SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+        }
+    }
+}
